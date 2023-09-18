074b3e794bdbd558eb2766cc66678f32361383b9
kaianndev <kaiannlobo@gmail.com>
2023-09-18 23:41:44 +0200

    meu primeiro projeto

---
+++ desafio01.js
@@ -0,0 +1,46 @@
+
+//Desafio de projetos 1
+// Aluno Desenvolvedor: Tiago Azevedo
+
+// Variáveis
+let cavaleiro = ["Seya de Pegasus" , "Shiryu de Dragao" , "Hades" , "Aldebaran de Touro", "Shaka de virgen", "Saori Kido(Atena)"]
+let cosmoCavaleiro = [ 3950, 5030, 6000, 7500, 9999, 1000000 ]
+let ArmaduradoCavaleiro = [" "," "," "," "," "," ",]
+
+// Aplicacao do uso de laços de repetição
+for ( let i = 0; i < cavaleiro.length; i++){
+    
+    let nome = cavaleiro[i]
+    let xp = cosmoCavaleiro[i]
+    let nivel = ArmaduradoCavaleiro[i] 
+
+    // Aplicando estruturas de decisão
+    if (xp < 1000){
+        nivel = "Ferro"
+
+    } else if(xp >= 1001 && xp <= 2000) {
+        nivel = "Bronze"
+
+    } else if (xp >= 2001 && xp <= 5000){
+        nivel = "Prata"
+
+    } else if (xp >= 5001 && xp <= 7000) {
+        nivel = "Ouro"
+
+    } else if (xp >= 7001 && xp <= 8000) {
+        nivel = "Platina"
+
+    } else if (xp >= 8001 && xp <= 9000) {
+        nivel = "Ascendente"
+
+    } else if (xp >= 9001 && xp <= 10000){
+        nivel = "Imortal"
+
+    } else if (xp >= 10001){
+        nivel = "Radiante"
+    }
+
+    // Saída
+    console.log(`Seu Cavalheiro e ${nome} Armadura de ${nivel} e explode cosmo em ${xp}`)
+}
+
